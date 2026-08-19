215d0a3877ed024dec5f6e62344838a0dd2c90cd
vamshiinet <vamshiinet@gmail.com>
2026-08-19 12:15:26 +0530

    adding java microservices project

---
+++ apiGtApplication/src/test/java/info/inetsolv/ApiGtApplicationTests.java
@@ -0,0 +1,13 @@
+package info.inetsolv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ paymentMsApplication/src/main/java/info/inetsolv/PayController.java
@@ -0,0 +1,20 @@
+package info.inetsolv;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PayController {
+
+	@Value("${server.port}")
+	private String serverPort;
+	
+	@GetMapping("/say")
+	public String getMethodName() {
+		return new String("Hi I am from Payment MS:"+serverPort);
+		
+	}
+}
